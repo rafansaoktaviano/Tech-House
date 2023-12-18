7a56eccde9ab4310cc149f57de7da2499609beb6
Rafansa Oktaviano <ravansaoktaviano@gmail.com>
2023-12-18 17:04:49 +0700

    first commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header>
+      <h1>Tech House</h1>
+      <nav>
+        <ul>
+          <li>Services</li>
+          <li>Our Teams</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../../components/Nav/Nav";
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+};
+
+export default Home;
